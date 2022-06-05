fb26e8cfed073f15eb2d6dc8b06ca8717570249b
Arturo Valencia <arturo.valencia@trojans.dsu.edu>
2022-06-04 23:10:16 -0500

    Finished Shell Sort

--- advancedSort.cpp
+++ advancedSort.cpp
@@ -7,6 +7,8 @@ void randomArray (int array[], int size);
 void equalArray (int oldArray[], int newArray[], int size); 
 void printArray (int array[], int size);
 void insertionSort (int array[], int size);
+void shellSort (int array[], int size);
+void fillArraySorted (int array[], int size);
 
 #define smallSize 20
 #define largeSize 100000
@@ -15,10 +17,16 @@ int main() {
     int insertArray[smallSize];
     int quickArray[smallSize];
     int shellArray[smallSize];
+    int insertArrayLG[largeSize];
+    int quickArrayLG[largeSize];
+    int shellArrayLG[largeSize];
 
     randomArray(insertArray, smallSize);
     equalArray(insertArray, quickArray, smallSize);
     equalArray(insertArray, shellArray, smallSize);
+    randomArray(insertArrayLG, largeSize);
+    equalArray(insertArrayLG, quickArrayLG, largeSize);
+    equalArray(insertArrayLG, shellArrayLG, largeSize);
 
     cout << "INSERTION SORT" << endl << endl << "Original array \n";
     printArray(insertArray, smallSize);
@@ -27,23 +35,45 @@ int main() {
     cout << "Array after sorting\n";
     printArray(insertArray, smallSize);
     cout << endl << endl << "------------------------------------------------------------------------------------------" << endl << endl;
-    // clock_t start, end;
-    // double insertTime;
-    // start = clock();
-    // insertionSort(insertArray, smallSize);
-    // end = clock();
-    // insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-    // cout << "Array after sorting" << endl;
-    // printArray(insertArray, smallSize);
-    // cout << endl << endl << "Time it took to sort: " << insertTime << "ms" << endl << "----------------------------------------" << endl << endl << endl;
 
-    cout << "Original quick sort array \n";
-    printArray(quickArray, smallSize);
-    cout << endl << endl;
-
-    cout << "Original shell sort array \n";
+    cout << "SHELL SORT" << endl << endl << "Original array \n";
     printArray(shellArray, smallSize);
     cout << endl << endl;
+    shellSort(shellArray, smallSize);
+    cout << "Array after sorting\n";
+    printArray(insertArray, smallSize);
+    cout << endl << endl << "------------------------------------------------------------------------------------------" << endl << endl << "Sorting Arrays size 100000" << endl;
+
+    clock_t start, end;
+    double insertTime;
+    start = clock();
+    insertionSort(insertArrayLG, largeSize);
+    end = clock();
+    insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    cout << "Time it took to sort using insertion: " << insertTime << " ms" << endl;
+
+    start = clock();
+    shellSort(shellArrayLG, largeSize);
+    end = clock();
+    insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    cout << "Time it took to sort using shell: " << insertTime << " ms" << endl;
+
+    fillArraySorted(insertArrayLG, largeSize);
+    fillArraySorted(shellArrayLG, largeSize);
+    cout << "Sorting arrays size 100000 where elements are 1, 2, 3...\n";
+
+    start = clock();
+    insertionSort(insertArrayLG, largeSize);
+    end = clock();
+    insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    cout << "Time it took to sort using insertion: " << insertTime << " ms" << endl;
+
+    start = clock();
+    shellSort(shellArrayLG, largeSize);
+    end = clock();
+    insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    cout << "Time it took to sort using shell: " << insertTime << " ms" << endl;
+
 
     return 0;
 }
@@ -86,5 +116,39 @@ void insertionSort (int array[], int size) {
         }
 
         array[walker+1] = hold;
+    }
+}
+
+
+void shellSort (int array[], int size) {
+    int current, hold, walker, incre, last;
+    last = size - 1;
+    incre = last/2;
+
+    while (incre != 0) {
+        current = incre;
+
+        while (current <= last) {
+            hold = array[current];
+            walker = current - incre;
+
+            while (walker >= 0 && hold < array[current]) {
+                array[walker + incre] = array[walker];
+                walker = walker - incre;
+            }
+            array[walker + incre] = hold;
+            current = current + 1;
+        }
+        incre = incre/2;
+    }
+}
+
+
+void fillArraySorted (int array[], int size) {
+    int i = 0, fill = 1;
+
+    for (i = 0 ; i < size ; ++i) {
+        array[i] = fill;
+        fill++;
     }
 }
