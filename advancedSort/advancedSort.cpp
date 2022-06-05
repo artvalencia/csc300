1c9319d7c39d96cebbb300bbfeb1e3d65da99c9d
artvalencia <97488566+artvalencia@users.noreply.github.com>
2022-06-05 16:44:01 -0500

    Finished

--- advancedSort/advancedSort.cpp
+++ advancedSort/advancedSort.cpp
@@ -9,6 +9,8 @@ void printArray (int array[], int size);
 void insertionSort (int array[], int size);
 void shellSort (int array[], int size);
 void fillArraySorted (int array[], int size);
+void quicksort (int array[], int p, int r);
+int partition (int array[], int p, int r);
 
 #define smallSize 20
 #define largeSize 100000
@@ -20,6 +22,8 @@ int main() {
     int insertArrayLG[largeSize];
     int quickArrayLG[largeSize];
     int shellArrayLG[largeSize];
+    clock_t start, end;
+    double insertTime;
 
     randomArray(insertArray, smallSize);
     equalArray(insertArray, quickArray, smallSize);
@@ -41,11 +45,17 @@ int main() {
     cout << endl << endl;
     shellSort(shellArray, smallSize);
     cout << "Array after sorting\n";
-    printArray(insertArray, smallSize);
-    cout << endl << endl << "------------------------------------------------------------------------------------------" << endl << endl << "Sorting Arrays size 100000" << endl;
+    printArray(shellArray, smallSize);
+    cout << endl << endl << "------------------------------------------------------------------------------------------" << endl << endl;
 
-    clock_t start, end;
-    double insertTime;
+    cout << "QUICK SORT" << endl << endl << "Original array \n";
+    printArray(quickArray, smallSize);
+    cout << endl << endl;
+    quicksort(quickArray, 0, smallSize - 1);
+    cout << "Array after sorting\n";
+    printArray(quickArray, smallSize);
+    cout << endl << endl << "------------------------------------------------------------------------------------------" << endl << endl << "Sorting Arrays size 100000 with random elements" << endl << endl;
+
     start = clock();
     insertionSort(insertArrayLG, largeSize);
     end = clock();
@@ -57,10 +67,17 @@ int main() {
     end = clock();
     insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
     cout << "Time it took to sort using shell: " << insertTime << " ms" << endl;
+
+    start = clock();
+    quicksort(quickArrayLG, 0, largeSize - 1);
+    end = clock();
+    insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    cout << "Time it took to sort using quicksort: " << insertTime << " ms" << endl << endl;
 
     fillArraySorted(insertArrayLG, largeSize);
     fillArraySorted(shellArrayLG, largeSize);
-    cout << "Sorting arrays size 100000 where elements are 1, 2, 3...\n";
+    fillArraySorted(quickArrayLG, largeSize);
+    cout << "Sorting arrays size 100000 where elements are pre-sorted (1, 2, 3...)\n\n";
 
     start = clock();
     insertionSort(insertArrayLG, largeSize);
@@ -74,6 +91,11 @@ int main() {
     insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
     cout << "Time it took to sort using shell: " << insertTime << " ms" << endl;
 
+    start = clock();
+    quicksort(quickArrayLG, 0, largeSize - 1);
+    end = clock();
+    insertTime = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+    cout << "Time it took to sort using quicksort: " << insertTime << " ms" << endl;
 
     return 0;
 }
@@ -132,7 +154,7 @@ void shellSort (int array[], int size) {
             hold = array[current];
             walker = current - incre;
 
-            while (walker >= 0 && hold < array[current]) {
+            while (walker >= 0 && hold < array[walker]) {
                 array[walker + incre] = array[walker];
                 walker = walker - incre;
             }
@@ -151,4 +173,32 @@ void fillArraySorted (int array[], int size) {
         array[i] = fill;
         fill++;
     }
+}
+
+
+void quicksort (int array[], int p, int r) {
+    int q = 0;
+
+    if (p < r) {
+        q = partition(array, p, r);
+        quicksort(array, p, q - 1);
+        quicksort(array, q + 1, r);            
+    }
+}
+
+
+int partition (int array[], int p, int r) {
+    int x = 0, i = 0, j = 0, hold = 0;
+    x = array[r];
+    i = p - 1;
+
+    for (j = p ; j < r ; ++j) {
+        if (array[j] <= x) {
+            i = i + 1;
+            swap(array[i], array[j]);
+        }
+    } 
+    swap(array[i + 1], array[r]);
+    
+    return i + 1;
 }
